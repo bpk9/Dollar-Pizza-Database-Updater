e3363871fe6def4f28576733e099999fbe6225ee
Brian P Kasper <31779860+bpk9@users.noreply.github.com>
2018-06-13 04:04:59 +0000

    Remove Google Geolocating

--- DollarPizzaFinder.js
+++ DollarPizzaFinder.js
@@ -2,106 +2,84 @@
 	Initialize Firebase Database
 */
 const config = {
-    apiKey: "AIzaSyBkw8JCB6ah8qlDmp7FhEC07x7Y9YSeu8Y",
-    authDomain: "dpf8790.firebaseapp.com",
-    databaseURL: "https://dpf8790.firebaseio.com",
-    projectId: "dpf8790",
-    storageBucket: "dpf8790.appspot.com",
-    messagingSenderId: "783284319998"
-  };
+		apiKey: "AIzaSyBkw8JCB6ah8qlDmp7FhEC07x7Y9YSeu8Y",
+		authDomain: "dpf8790.firebaseapp.com",
+		databaseURL: "https://dpf8790.firebaseio.com",
+		projectId: "dpf8790",
+		storageBucket: "dpf8790.appspot.com",
+		messagingSenderId: "783284319998"
+};
 firebase.initializeApp(config);
+
+/*
+	Variables
+*/
+var placeName;
+var placeId;
 
 /*
 	Initialize Google Map
 */
+function initMap() {  
+  	// The map, centered at Manhattan
+  const map = new google.maps.Map(document.getElementById('map'), {zoom: 11, center: {lat: 40.7831, lng: -73.9712}});
 
-function initMap() {
-  // The location of Uluru
-  var uluru = {lat: -25.344, lng: 131.036};
-  
-  // The map, centered at Uluru
-  var map = new google.maps.Map(
-      document.getElementById('map'), {zoom: 4, center: uluru});
-  
-  // The marker, positioned at Uluru
-  var marker = new google.maps.Marker({position: uluru, map: map});
+  // The marker w/ info window
+  var infowindow = new google.maps.InfoWindow();
+  const marker = new google.maps.Marker({map: map});
 
   // autocomplete search box
   var autocomplete = new google.maps.places.Autocomplete(document.getElementById('pac-input'));
-  autocomplete.bindTo('bounds', map);
   autocomplete.addListener('place_changed', function() {
-          marker.setVisible(false);
-          var place = autocomplete.getPlace();
-          if (!place.geometry) {
+  	marker.setVisible(false);
+  	const place = autocomplete.getPlace();
+  	if (!place.geometry) {
             // User entered the name of a Place that was not suggested and
             // pressed the Enter key, or the Place Details request failed.
-            window.alert("No details available for input: '" + place.name + "'");
+            window.alert("No details available for input: " + place.name);
             return;
-          }
+        }
 
           // If the place has a geometry, then present it on a map.
           if (place.geometry.viewport) {
-            map.fitBounds(place.geometry.viewport);
+          	map.fitBounds(place.geometry.viewport);
           } else {
-            map.setCenter(place.geometry.location);
+          	map.setCenter(place.geometry.location);
             map.setZoom(17);  // Why 17? Because it looks good.
-          }
-          marker.setPosition(place.geometry.location);
-          marker.setVisible(true);
+        }
 
-          var address = '';
-          if (place.address_components) {
-            address = [
-              (place.address_components[0] && place.address_components[0].short_name || ''),
-              (place.address_components[1] && place.address_components[1].short_name || ''),
-              (place.address_components[2] && place.address_components[2].short_name || '')
-            ].join(' ');
-          }
-          
+        placeName = place.name;
+        placeId = place.place_id;
+        placeLocation = place.geometry.location;
+        placeStreet = place.address_components[1]['long_name'];
+
+        marker.setPosition(place.geometry.location);
+
+        google.maps.event.addListener(marker, 'click', function() {
+        	infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + place.formatted_address + '</div>');
+        	infowindow.open(map, this);
         });
+
+        marker.setVisible(true);
+
+    });
 }
 
 /*
 	Called from 'Add to Database' button
 */
 function addToDatabase() {
-	// get place id from text box
-	var placeId = document.getElementById('placeId').value;
-	var placeName = document.getElementById('placeName').value;
 
-	if (placeId == "" || placeName == "") {
-		alert("Name and ID fields must both be filled.");
-		return;
+	if (placeId == "") {
+		alert("Search for a place to add!");
+	} else {
+		// set data in firebase
+		firebase.database().ref('locations/' + placeName + " - " + placeStreet).set({
+			latitude: placeLocation.lat(),
+			longitude: placeLocation.lng(),
+			placeId: placeId
+		});
+		alert(placeName + " successfully added to database!")
 	}
 
-	// get data from google geocoding
-	const geocoder = new google.maps.Geocoder();
-	geocoder.geocode({'placeId': placeId}, function(results, status) {
-		if (status == "OK") {
-			const place = results[0];
-
-			// set data in firebase
-			firebase.database().ref('locations/' + placeName).set({
-				latitude: place.geometry.location.lat(),
-				longitude: place.geometry.location.lng(),
-				placeId: place.place_id
-			});
-
-			// confirmation message
-			alert(placeName + " was successfully added to the database!");
-
-			// clear text box
-			clear();
-		} else {
-			alert("Google Geocoder Error: " + status);
-		}
-	});
-}
-
-/*
-	Clears text from text boxes
-*/
-function clear() {
-	document.getElementById('placeId').value = "";
-	document.getElementById('placeName').value = "";
 }
